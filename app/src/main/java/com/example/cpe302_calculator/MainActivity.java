cddd29e2f33b03c5aab573b81c15d62c936b94e0
horllan <makanjuolafunmibi@gmail.com>
2023-01-03 08:18:47 +0100

    added code to initialize the buttons

--- app/src/main/java/com/example/cpe302_calculator/MainActivity.java
+++ app/src/main/java/com/example/cpe302_calculator/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.cpe302_calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnClr, btnAdd, btnMinus, btnDel, btnMul, btnM_Minus, btnM_Add, btnM_R, btnM_C;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize all the button by setting them to the respective resource id
+        initializeBtn();
+    }
+
+    private void initializeBtn(){
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnAdd = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnDiv);
+        btnClr = findViewById(R.id.btnClr);
+        btnDel = findViewById(R.id.btnDel);
+        btnMul = findViewById(R.id.btnMul);
+        btnM_C = findViewById(R.id.btnM_C);
+        btnM_Minus = findViewById(R.id.btnM_Minus);
+        btnM_R = findViewById(R.id.btnM_R);
+        btnM_R = findViewById(R.id.btnM_Plus);
+        btnM_Add = findViewById(R.id.btnM_R);
     }
 }
